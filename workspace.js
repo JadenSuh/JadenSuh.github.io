aa13ab2f0f3128f1abfbb5054b46d6d5da661ed3
Jaden Suh <92055248+JadenSuh@users.noreply.github.com>
2023-10-02 00:58:23 -0400

    Update workspace.js
    testing removal of excess code

--- workspace.js
+++ workspace.js
@@ -1,89 +1,92 @@
 /* Define custom blocks using Blockly.Blocks object */
-
-  Blockly.Blocks['compound'] = {
-	init: function() {
-	  this.appendValueInput("ELEMENT1")
-		  .setCheck(["Element", "Compound"])
-		  .appendField("Compound:");
-	  this.appendValueInput("ELEMENT2")
-		  .setCheck(["Element", "Compound"]);
-	  this.setInputsInline(true);
-	  this.setOutput(true, "Compound");
-	  this.setColour(120);
-   this.setTooltip("");
-   this.setHelpUrl("");
-	}
-  };
   
-  Blockly.Blocks['element_symbol'] = {
+Blockly.Blocks['element_symbol'] = {
 	init: function() {
-	  this.appendValueInput("SYMBOL")
+		this.appendValueInput("SYMBOL")
 		  .setCheck("String")
-		  .appendField("Element Symbol")
+		  .appendField("Ele") /* Name that appears on block piece */
 		  .appendField(new Blockly.FieldTextInput("H"), "NAME");
-	  this.setInputsInline(false);
-	  this.setOutput(true, "Element");
-	  this.setColour(120);
-   this.setTooltip("Enter the chemical symbol for the desired element");
-   this.setHelpUrl("https://en.wikipedia.org/wiki/Chemical_symbol");
+		this.setInputsInline(false);
+		this.setOutput(true, "Element"); /* output type to allow block connections */
+		this.setColour(120);
+		this.setTooltip("Enter the chemical symbol for the desired element");
+		this.setHelpUrl("https://en.wikipedia.org/wiki/Chemical_symbol");
 	}
-  };
-  
-  Blockly.Blocks['element_coefficient'] = {
+};
+
+Blockly.Blocks['element_coefficient'] = {
 	init: function() {
-	  this.appendValueInput("NAME")
+		this.appendValueInput("COEFFICIENT")
 		  .setCheck("Element")
-		  .appendField(new Blockly.FieldNumber(1, 1), "NAME");
-	  this.setInputsInline(true);
-	  this.setOutput(true, "Element");
-	  this.setColour(180);
-   this.setTooltip("Type the coefficient for the number the inserted element");
-   this.setHelpUrl("");
+		  .appendField(new Blockly.FieldNumber(1, 1), "NUMBER");
+		this.setInputsInline(true);
+		this.setOutput(true, "Element");
+		this.setColour(180);
+		this.setTooltip("Type the coefficient for the number the inserted element");
+		this.setHelpUrl("");
 	}
-  };
-  
-  Blockly.Blocks['chemical_equation'] = {
+};
+
+Blockly.Blocks['compound'] = {
 	init: function() {
-	  this.appendValueInput("REACTANTS")
+		this.appendValueInput("ELEMENT1")
+		  .setCheck(["Element", "Compound"])
+		  .appendField("Comp");
+		this.appendValueInput("ELEMENT2")
+		  .setCheck(["Element", "Compound"]);
+		this.setInputsInline(true);
+		this.setOutput(true, "Compound");
+		this.setColour(120);
+		this.setTooltip("Create a compound by inserting two elements");
+		this.setHelpUrl("");
+	}
+};
+  
+Blockly.Blocks['chemical_equation'] = {
+	init: function() {
+		this.appendValueInput("REACTANTS")
 		  .setCheck(["EquationInput", "Element", "Compound"]);
-	  this.appendDummyInput()
+		this.appendDummyInput()
 		  .appendField(new Blockly.FieldDropdown([["→","FORWARD"], ["⇌","EQUILIBRIUM"]]), "EquationType");
-	  this.appendValueInput("PRODUCTS")
+		this.appendValueInput("PRODUCTS")
 		  .setCheck(["EquationInput", "Element", "Compound"]);
-	  this.setInputsInline(true);
-	  this.setOutput(true, "EquationOutput");
-	  this.setColour(230);
-   this.setTooltip("Insert reactants_or_products onto each side");
-   this.setHelpUrl("");
+		this.setInputsInline(true);
+		this.setOutput(true, "EquationOutput");
+		this.setColour(230);
+		this.setTooltip("Insert reactants_or_products onto each side");
+		this.setHelpUrl("");
 	}
-  };
+};
   
-  Blockly.Blocks['reactants_or_products'] = {
+Blockly.Blocks['reactants_or_products'] = {
 	init: function() {
-	  this.appendValueInput("Element1")
+		this.appendValueInput("Element1")
 		  .setCheck(["Element", "Compound"]);
-	  this.appendDummyInput()
+		this.appendDummyInput()
 		  .appendField("+");
-	  this.appendValueInput("Element2")
+		this.appendValueInput("Element2")
 		  .setCheck(["Element", "Compound"]);
-	  this.setInputsInline(true);
-	  this.setOutput(true, "EquationInput");
-	  this.setColour(230);
-   this.setTooltip("");
-   this.setHelpUrl("");
+		this.setInputsInline(true);
+		this.setOutput(true, "EquationInput");
+		this.setColour(230);
+		this.setTooltip("");
+		this.setHelpUrl("");
 	}
-  };
+};
 
 /* Code Generator Functions */
 
 Blockly.JavaScript['element_symbol'] = function(block) {
-  var name = block.getFieldValue('NAME');
-  return [name, Blockly.JavaScript.ORDER_NONE];
+	var var_name = block.getFieldValue('NAME');
+	var code = var_name;
+	return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
 Blockly.JavaScript['element_coefficient'] = function(block) {
-  var name = block.getFieldValue('NAME');
-  return [name, Blockly.JavaScript.ORDER_NONE];
+	var num = block.getFieldValue('NUMBER');
+	var ele_name = Blockly.JavaScript.valueToCode(block, 'COEFFICIENT', Blockly.JavaScript.ORDER_NONE);
+	var code = num + ele_name;
+  return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['compound'] = function(block) {
@@ -134,14 +137,10 @@ var options = {
 	sounds : true, 
 	oneBasedIndex : true
 };
-
-/* Inject your workspace */ 
+/*
 var workspace = Blockly.inject('blocklyDiv', options);
 
-/* Load Workspace Blocks from XML to workspace. Remove all code below if no blocks to load */
-
-/* TODO: Change workspace blocks XML ID if necessary. Can export workspace blocks XML from Workspace Factory. */
 var workspaceBlocks = document.getElementById("toolbox"); 
 
-/* Load blocks to workspace. */
 Blockly.Xml.domToWorkspace(workspaceBlocks, workspace);
+*/
